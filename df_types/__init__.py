0cc1c9a3dd1268b267066d7d87a52a6fc682ec9a
jon-edward <townsend.jonathan.e@gmail.com>
2025-07-27 12:07:36 -0400

    Initial commits

---
+++ df_types/__init__.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass, field
+
+import pandas as pd
+
+from df_types._infer import infer_types
+from df_types._codegen import generate_types
+from df_types._util import normalize_columns
+
+from df_types.config import DFTypesConfig
+
+
+@dataclass
+class DFTypes:
+    df: pd.DataFrame
+    config: DFTypesConfig = field(default_factory=DFTypesConfig)
+
+    def __post_init__(self):
+        self.df = _sample_df(self.df, self.config)
+
+    def generate_type_file(self) -> None:
+        col_to_attr_names = {
+            col: name
+            for col, name in zip(
+                self.df.columns, normalize_columns(self.df.columns.tolist())
+            )
+        }
+        types = {col: infer_types(self.df[col], self.config) for col in self.df.columns}
+
+        output_file = self.config.output_file
+        if isinstance(output_file, str) and not output_file.endswith(".py"):
+            output_file += ".py"
+
+        with open(self.config.output_file, "w") as f:
+            f.write(generate_types(col_to_attr_names, types, self.config))
+
+
+def _sample_df(df: pd.DataFrame, config: DFTypesConfig) -> pd.DataFrame:
+    """
+    Returns a sample of the DataFrame
+    """
+
+    head_rows = config.sample_head_rows
+    tail_rows = config.sample_tail_rows
+    middle_rows = config.sample_middle_rows
+
+    if len(df.index) <= head_rows + middle_rows + tail_rows:
+        return df
+
+    head_sample = df.head(head_rows)
+    middle_sample = df.iloc[head_rows:-tail_rows].sample(middle_rows)
+    tail_sample = df.tail(tail_rows)
+
+    return pd.concat([head_sample, middle_sample, tail_sample])
---
+++ df_types/config.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import numpy as np
+
+
+@dataclass
+class DFTypesConfig:
+    sample_head_rows: int = 10
+    """
+    Number of rows to sample from the head of the DataFrame to infer types
+    """
+
+    sample_middle_rows: int = 1_000
+    """
+    Number of rows to sample from the middle of the DataFrame to infer types
+    """
+
+    sample_tail_rows: int = 10
+    """
+    Number of rows to sample from the tail of the DataFrame to infer types
+    """
+
+    max_literal_size: int = 10
+    """
+    Maximum number of literals to infer
+    """
+
+    max_literal_repr_len: int = 100
+    """
+    Maximum cumulative repr size of all literals
+    """
+
+    random_state: np.random.RandomState | int = field(
+        default_factory=np.random.RandomState
+    )
+    """
+    Random state to use for sampling
+    """
+
+    output_file: str | Path = "typed_df.py"
+    """
+    Output file path
+    """
+
+    class_name: str = "TypedDF"
+    """
+    Name for the generated class
+    """
+
+    infer_literals: bool = True
+    """
+    Whether to infer literals
+    """
+
+    use_slots: bool = True
+    """
+    Whether to use __slots__ in the generated class
+    """
